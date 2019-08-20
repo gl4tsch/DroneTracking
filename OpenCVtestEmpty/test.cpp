40bfd9f8e889a596da0c5247be95af12a633af5a
Flo Laptop <florian.hoeschl@googlemail.com>
2019-08-21 00:20:31 +0200

    filled out camshift

--- OpenCVtestEmpty/test.cpp
+++ OpenCVtestEmpty/test.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 using namespace cv;
 
+int imgSizeX = 640;
+int imgSizeY = 360;
 Mat imgOriginal;
 Mat imgHLS;
 Mat imgThresholded;
@@ -14,15 +16,27 @@ vector<Vec4i> hierarchy;
 
 int lowH = 0;
 int highH = 180;
-
 int lowS = 0;
 int highS = 255;
-
 int lowV = 0;
 int highV = 255;
-
 int lowL = 0;
 int highL = 255;
+
+//camshift
+bool selectObject = false;
+Rect selection;
+Point origin;
+int trackObject = 0;
+int vmin = 10, vmax = 256, smin = 30;
+Rect trackWindow;
+int hsize = 16;
+float hranges[] = { 0,180 };
+const float* phranges = hranges;
+Mat frame, hsv, hue, mask, hist, histimg = Mat::zeros(200, 320, CV_8UC3), backproj;
+bool paused = false;
+bool backprojMode = false;
+bool showHist = true;
 
 
 
@@ -93,7 +107,113 @@ void detectHLSthresholds() {
 }
 
 void trackCamshift() {
+	if (!paused)
+	{
+		cvtColor(imgOriginal, hsv, COLOR_BGR2HSV);
+		if (trackObject)
+		{
+			int _vmin = vmin, _vmax = vmax;
+			inRange(hsv, Scalar(0, smin, MIN(_vmin, _vmax)),
+				Scalar(180, 256, MAX(_vmin, _vmax)), mask);
+			int ch[] = { 0, 0 };
+			hue.create(hsv.size(), hsv.depth());
+			mixChannels(&hsv, 1, &hue, 1, ch, 1);
+			if (trackObject < 0)
+			{
+				// Object has been selected by user, set up CAMShift search properties once
+				Mat roi(hue, selection), maskroi(mask, selection);
+				calcHist(&roi, 1, 0, maskroi, hist, 1, &hsize, &phranges);
+				normalize(hist, hist, 0, 255, NORM_MINMAX);
+				trackWindow = selection;
+				trackObject = 1; // Don't set up again, unless user selects new ROI
+				histimg = Scalar::all(0);
+				int binW = histimg.cols / hsize;
+				Mat buf(1, hsize, CV_8UC3);
+				for (int i = 0; i < hsize; i++)
+					buf.at<Vec3b>(i) = Vec3b(saturate_cast<uchar>(i*180. / hsize), 255, 255);
+				cvtColor(buf, buf, COLOR_HSV2BGR);
+				for (int i = 0; i < hsize; i++)
+				{
+					int val = saturate_cast<int>(hist.at<float>(i)*histimg.rows / 255);
+					rectangle(histimg, Point(i*binW, histimg.rows),
+						Point((i + 1)*binW, histimg.rows - val),
+						Scalar(buf.at<Vec3b>(i)), -1, 8);
+				}
+			}
+			// Perform CAMShift
+			calcBackProject(&hue, 1, 0, hist, backproj, &phranges);
+			backproj &= mask;
+			RotatedRect trackBox = CamShift(backproj, trackWindow,
+				TermCriteria(TermCriteria::EPS | TermCriteria::COUNT, 10, 1));
+			if (trackWindow.area() <= 1)
+			{
+				int cols = backproj.cols, rows = backproj.rows, r = (MIN(cols, rows) + 5) / 6;
+				trackWindow = Rect(trackWindow.x - r, trackWindow.y - r,
+					trackWindow.x + r, trackWindow.y + r) &
+					Rect(0, 0, cols, rows);
+			}
+			if (backprojMode)
+				cvtColor(backproj, imgOriginal, COLOR_GRAY2BGR);
+			ellipse(imgOriginal, trackBox, Scalar(0, 0, 255), 3, LINE_AA);
+		}
+	}
+	else if (trackObject < 0)
+		paused = false;
+	if (selectObject && selection.width > 0 && selection.height > 0)
+	{
+		Mat roi(imgOriginal, selection);
+		bitwise_not(roi, roi);
+	}
+	imshow("Histogram", histimg);
+	char c = (char)waitKey(10);
+	switch (c)
+	{
+	case 'b':
+		backprojMode = !backprojMode;
+		break;
+	case 'c':
+		trackObject = 0;
+		histimg = Scalar::all(0);
+		break;
+	case 'h':
+		showHist = !showHist;
+		if (!showHist)
+			destroyWindow("Histogram");
+		else
+			namedWindow("Histogram", 1);
+		break;
+	case 'p':
+		paused = !paused;
+		break;
+	default:
+		;
+	}
+}
 
+// User draws box around object to track. This triggers CAMShift to start tracking
+static void onMouse(int event, int x, int y, int, void*)
+{
+	if (selectObject)
+	{
+		selection.x = MIN(x, origin.x);
+		selection.y = MIN(y, origin.y);
+		selection.width = std::abs(x - origin.x);
+		selection.height = std::abs(y - origin.y);
+		selection &= Rect(0, 0, imgOriginal.cols, imgOriginal.rows);
+	}
+	switch (event)
+	{
+	case EVENT_LBUTTONDOWN:
+		origin = Point(x, y);
+		selection = Rect(x, y, 0, 0);
+		selectObject = true;
+		break;
+	case EVENT_LBUTTONUP:
+		selectObject = false;
+		if (selection.width > 0 && selection.height > 0)
+			trackObject = -1;   // Set up CAMShift properties in main() loop
+		break;
+	}
 }
 
 int main()
@@ -108,10 +228,17 @@ int main()
 		return -1;
 	}
 
-	cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
-	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 360);
+	cap.set(CV_CAP_PROP_FRAME_WIDTH, imgSizeX);
+	cap.set(CV_CAP_PROP_FRAME_HEIGHT, imgSizeY);
 
 	createTrackbars();
+
+	//camshift temp
+	namedWindow("Original", 1);
+	setMouseCallback("Original", onMouse, 0);
+	createTrackbar("Vmin", "Original", &vmin, 256, 0);
+	createTrackbar("Vmax", "Original", &vmax, 256, 0);
+	createTrackbar("Smin", "Original", &smin, 256, 0);
 
 	while (true)
 	{
@@ -129,7 +256,7 @@ int main()
 			cout << "Video loop" << endl;
 		}
 
-		resize(imgOriginal, imgOriginal, Size(640, 360), 0, 0, INTER_CUBIC); //resize to 640 by 360
+		resize(imgOriginal, imgOriginal, Size(imgSizeX, imgSizeY), 0, 0, INTER_CUBIC); //resize to 640 by 360
 
 		//detectHLSthresholds(); //show regions of specified HLS values
 
@@ -137,10 +264,10 @@ int main()
 
 		imshow("Original", imgOriginal); //show the original image
 
-		//if (waitKey(0) == 32) //frame by frame with 'space'
-		//{
-		//	cout << "space key is pressed by user" << endl;
-		//}
+		if (waitKey(0) == 32) //frame by frame with 'space'
+		{
+			cout << "space key is pressed by user" << endl;
+		}
 		if (waitKey(1) == 27) //wait for 'esc' key (27) press => break loop
 		{
 			cout << "esc key is pressed by user" << endl;
